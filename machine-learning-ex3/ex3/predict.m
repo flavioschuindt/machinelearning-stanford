c94fd63c978f43d769bf5fdf4aa6b82ce1db998a
Flávio Schuindt <flavio.schuindt@gmail.com>
2015-10-12 00:12:19 -0300

    - Adding Neural Network Predicition Function

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -20,7 +20,13 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
-
+a1 = [ones(m, 1) X]; % 5000 x 401
+z2 = Theta1 * a1'; % 25 x 5000
+a2 = sigmoid(z2); % 25 x 5000
+a2 = [ones(1, size(a2,2)); a2]; % 26 x 5000 
+z3 = Theta2 * a2; % 10 x 5000
+a3 = sigmoid(z3); % 10 x 5000
+[x, p] = max(a3', [], 2);
 
 
 
